315f486f70e52cc2b42b76fbcc6f850857075ee1
Vlod Kalicun <vlod@vlod.com>
2023-09-24 13:05:30 -0600

    remove unused test

--- __tests__/sum.spec.js
+++
@@ -1,9 +0,0 @@
-//@ts-check
-import { expect, describe, it } from "vitest";
-import { sum } from "../src/sum";
-
-describe("sum", () => {
-  it("should calculate additions", () => {
-    expect(sum(11, 31)).toBe(42);
-  });
-});
